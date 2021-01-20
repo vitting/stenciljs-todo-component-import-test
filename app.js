31cd1d7e547ecda99632450129ff36862a4f2c1d
Christian Nicolaisen <cvn_vitting@hotmail.com>
2021-01-20 18:31:50 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+// import "./node_modules/ghost-todo-stenciljs-webcomponent/dist/ghost-todo/ghost-todo.esm.js";
+import {defineCustomElements} from "ghost-todo-stenciljs-webcomponent";
+defineCustomElements();
